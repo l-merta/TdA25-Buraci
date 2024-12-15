04990903eee677b371fa74a278dc576482c81874
l-merta <lukas.merta26@gmail.com>
2024-12-15 01:07:05 +0100

    Upload error fix

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { connectToDatabase, getDb, closeDatabase } = require("./db");
 const { v4: uuidv4 } = require("uuid");
-require("dotenv").config();
+require("dotenv").config({ path: '/server/.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5200;
