33a58f93ad4048699e42d744dcc7d19e9fc0c73b
l-merta <lukas.merta26@gmail.com>
2024-12-15 23:18:56 +0100

    GameList component

---
+++ client/src/pages/Games.tsx
@@ -0,0 +1,12 @@
+import GameList from './../components/GameList';
+
+const Games = () => {
+  return (
+    <>
+      <h1>Uložené hry piškvorek</h1>
+      <GameList></GameList>
+    </>
+  )
+}
+
+export default Games
