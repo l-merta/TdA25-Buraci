adbc28a078d90539a15019a6263a286475e4e512
l-merta <lukas.merta26@gmail.com>
2024-12-22 00:14:23 +0100

    Create page Link bug fix

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -5,7 +5,7 @@ import GameBoard from "./../components/GameBoard";
 const Create = () => {
   return (
     <>
-      <Link to="games/">Games</Link>
+      <Link to="/games">Games</Link>
       <h1>Create page</h1>
       <GameBoard size={15} editMode={true}/>
     </>
