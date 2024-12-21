34a322c1c91c45a353fd3693919be3cb8b6e4ff6
l-merta <lukas.merta26@gmail.com>
2024-12-21 23:31:53 +0100

    Removing WebSockets

--- client/src/components/Socket.ts
+++
@@ -1,9 +0,0 @@
-// src/socket.ts
-import { io, Socket } from "socket.io-client";
-
-const SOCKET_URL = import.meta.env.VITE_WS_URL || `${window.location.protocol}//${window.location.hostname}`;
-console.log(SOCKET_URL);
-
-const socket: Socket = io(SOCKET_URL);
-
-export default socket;
