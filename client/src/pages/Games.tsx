8c9d4177869b58fc8f5d2e3997db2da1460c588f
l-merta <lukas.merta26@gmail.com>
2025-01-11 17:22:27 +0100

    Games design

--- client/src/pages/Games.tsx
+++ client/src/pages/Games.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import GameList from './../components/GameList';
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+import GameList from './../components/GameList';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
 
@@ -18,6 +18,18 @@ const Games = () => {
       gameMode: "ai",
       playerNames: ["Hráč 1", "Táda"],
       ai: [0, 1]
+    },
+    {
+      id: 2,
+      gameMode: "online",
+      playerNames: ["Hráč 1", "Hráč 2"],
+      ai: [0, 0]
+    },
+    {
+      id: 3,
+      gameMode: "spectate",
+      playerNames: ["Táda", "Pišk-GPT"],
+      ai: [1, 1]
     }
   ];
 
@@ -33,18 +45,36 @@ const Games = () => {
         <div className="game-modes">
           <button className={"mode " + (gameSett.id === 0 ? "mode-active " : " ")} onClick={() => setGameSett(gameSettArr[0])}>
             <h3>
-              <span>Lokální hra</span>
               <i className="fa-regular fa-computer-classic"></i>
+              <span>Lokální hra</span>
             </h3>
             <p>Zahraj si hru piškvorek na jednom počítači</p>
           </button>
           <button className={"mode " + (gameSett.id === 1 ? "mode-active " : " ")} onClick={() => setGameSett(gameSettArr[1])}>
             <h3>
-              <span>Proti AI</span>
               <i className="fa-regular fa-microchip-ai"></i>
+              <span>Proti AI</span>
             </h3>
-            <p>Zkus porazit našeho AI šampióna Tádu!</p>
+            <p>Zkus porazit našeho AI šampióna Tádu</p>
           </button>
+          <button className={"mode " + (gameSett.id === 2 ? "mode-active " : " ")} onClick={() => setGameSett(gameSettArr[2])}>
+            <h3>
+              <i className="fa-regular fa-globe"></i>
+              <span>Online</span>
+            </h3>
+            <p>Zasoupeř si se svými kamarády nebo rodinou</p>
+          </button>
+          <button className={"mode " + (gameSett.id === 3 ? "mode-active " : " ")} onClick={() => setGameSett(gameSettArr[3])}>
+            <h3>
+              <i className="fa-regular fa-glasses"></i>
+              <span>Divák</span>
+            </h3>
+            <p>Podívej se, jak Táda hájí svůj status šampióna proti jinému AI</p>
+          </button>
+        </div>
+        <div className="play-actions">
+          <Link to="/game/" state={gameSett} className="button button-1 button-red">Nová hra</Link>
+          <Link to="/create/" className="button button-1 button-red button-border">Vytvořit hru</Link>
         </div>
         <GameList gameSett={gameSett} />
       </div>
