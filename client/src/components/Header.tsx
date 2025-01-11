e5d661174b0b3f0d4686408e07f7c320a98f31b8
l-merta <lukas.merta26@gmail.com>
2025-01-11 23:55:03 +0100

    Burger menu

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'theme-light');
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     document.body.className = theme;
@@ -28,7 +29,6 @@ const Header = () => {
   function switchColorTheme() {
     const newTheme = theme === 'theme-light' ? 'theme-dark' : 'theme-light';
     setTheme(newTheme);
-    localStorage.setItem('theme', newTheme); // Ensure localStorage is updated after state change
   }
 
   return (
@@ -44,6 +44,25 @@ const Header = () => {
           <i className={theme === 'theme-light' ? 'fa-solid fa-moon' : 'fa-solid fa-sun-bright'}></i>
         </button>
         <Link to="/game" className='button button-red'>Nová hra</Link>
+      </div>
+      <div className="burger-menu">
+        <button className="theme-switch" onClick={switchColorTheme}>
+            <i className={theme === 'theme-light' ? 'fa-solid fa-moon' : 'fa-solid fa-sun-bright'}></i>
+        </button>
+        <button className="burger-button" onClick={() => setMenuOpen(!menuOpen)}>
+            <i className="fa-solid fa-bars"></i>
+        </button>
+        {menuOpen && (
+          <nav className="burger-nav">
+            <Link to="/games" className='button button-empty'>Seznam her</Link>
+            <Link to="/think-different-academy" className='button button-empty'>O TdA</Link>
+            <Link to="/about-team" className='button button-empty'>O týmu a aplikaci</Link>
+            <Link to="/game" className='button button-red'>Nová hra</Link>
+            {/* <button className="theme-switch" onClick={switchColorTheme}>
+              <i className={theme === 'theme-light' ? 'fa-solid fa-moon' : 'fa-solid fa-sun-bright'}></i>
+            </button> */}
+          </nav>
+        )}
       </div>
     </header>
   )
