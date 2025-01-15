f67effb3ba07dcc24d70d7a2694a3079bcfea348
degescigoma <robikodemon@gmail.com>
2025-01-15 22:35:13 +0100

    tda page

--- client/src/pages/Tda.tsx
+++ client/src/pages/Tda.tsx
@@ -14,6 +14,25 @@ function Tda() {
       <div className="bg-grad"></div>
       <div className="main-tda">
         <h1>Stránka o TdA</h1>
+        <section className='sec-tda'>
+          <h2>Think different academy</h2>
+            <h3 className=''>O nás</h3>
+              <div>
+                <p className='text-tda'>
+                Think Different Academy je nezisková organizace zaměřená na podporu rozvoje myšlení u studentů i širší veřejnosti. Cílem organizace je přinášet inovativní způsoby, jak zlepšit logické a analytické schopnosti jednotlivců. V současnosti se Think Different Academy soustředí na vývoj unikátní piškvorkové platformy, která digitalizuje tradiční hru a poskytuje uživatelům atraktivní herní zážitek.
+                </p>
+                <p className='text-tda'>
+                Platforma nabídne možnost hraní klasických piškvorek v režimu lokálního multiplayeru, čímž umožní hráčům užít si hru s přáteli a rodinou. Kromě toho bude zahrnovat také speciální piškvorkové úlohy, které poslouží k trénování a zlepšování herních dovedností. Tento prostor pro cvičení bude skvělou volbou pro všechny, kteří chtějí zdokonalit svou strategii a naučit se nové taktiky.
+                </p>
+                <p className='text-tda'>
+                Do budoucna plánuje Think Different Academy rozšířit svou nabídku o další logické hry, čímž chce nabídnout rozmanité výzvy pro všechny zájemce o rozvoj svého myšlení a zábavu.
+                </p>
+              </div>
+          <img src="/images/logos/Think-different-Academy_LOGO_oficialni.png" alt=""/>
+
+
+
+          </section>
       </div>
       <Footer />
     </>
