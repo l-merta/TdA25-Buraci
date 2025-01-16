f6838847070d81d9e1d5eac86fd152b19058d323
l-merta <lukas.merta26@gmail.com>
2025-01-16 20:56:02 +0100

    Pages text edit

--- client/src/pages/Team.tsx
+++ client/src/pages/Team.tsx
@@ -20,16 +20,16 @@ function Team() {
           <h3>Jak se vyvíjeli piškvorky</h3>
           <div className='text-team'>
               <p>
-                Naše aplikace pro piškvorky umožňuje hrát hru několika způsoby. V lokálním režimu si můžete zahrát proti kamarádovi na jednom počítači – stačí si sednout k monitoru a užít si klasickou hru. Pokud chcete hrát na dálku, nabízíme multiplayer režim, který využívá <span className='bold-text-team'>Socket.io</span> pro online komunikaci mezi hráči. To znamená, že můžete hrát proti přátelům, ať už jsou kdekoli na světě. Pokud nemáte proti komu hrát, můžete využít <span className='bold-text-team'>naši umělou inteligenci Tádou</span>, která je navržena tak, aby se učila a přizpůsobovala, což znamená, že vždy budete mít silného a zábavného soupeře.
+                Naše aplikace pro piškvorky umožňuje hrát hru několika způsoby. V lokálním režimu si můžete zahrát proti kamarádovi na jednom počítači – stačí si sednout k monitoru a užít si klasickou hru. Pokud chcete hrát na dálku, nabízíme multiplayer režim, který využívá <span className='bold-text-team'>Socket.io</span> pro online komunikaci mezi hráči. To znamená, že můžete hrát proti přátelům, ať už jsou kdekoli na světě. Pokud nemáte proti komu hrát, můžete využít <span className='bold-text-team'>naši umělou inteligenci Tádu</span>, který je navržen tak, aby se učil a přizpůsoboval, což znamená, že vždy budete mít silného a zábavného soupeře.
               </p>
               <p>
                 Kromě toho umožňuje aplikace sledování zápasů dvou AI botů, kde si můžete prohlédnout jejich strategii a získat inspiraci pro vlastní hru. Tento režim také funguje jako ideální nástroj pro trénink, pokud si chcete zlepšit své schopnosti.
               </p>
               <p>
-                Aplikace běží na moderních technologiích, které zajišťují její plynulý chod. Frontend je postaven na <span className='bold-text-team'>React a Vite</span>, což umožňuje rychlou a interaktivní uživatelskou zkušenost. Backend je postaven na <span className='bold-text-team'>Node.js s Expressem</span>, což poskytuje robustní a škálovatelnou infrastrukturu pro multiplayer a správu herních dat. Pro uchovávání dat využíváme <span className='bold-text-team'>MySQL databázi</span>, která nám umožňuje efektivně spravovat uživatelské účty a historii her.
+                Aplikace běží na moderních technologiích, které zajišťují její plynulý chod. Frontend je postaven na <span className='bold-text-team'>React a Vite</span>, což umožňuje rychlou a interaktivní uživatelskou zkušenost. Backend je postaven na <span className='bold-text-team'>Node.js s Expressem</span>, což poskytuje robustní a škálovatelnou infrastrukturu pro multiplayer a správu herních dat. Pro uchovávání dat využíváme <span className='bold-text-team'>MySQL databázi</span>, která nám umožňuje efektivně spravovat všechna herní data.
               </p>
               <p>
-                Celý projekt je zabaleno v <span className='bold-text-team'>Dockeru</span>, což nám umožňuje snadnou nasazení a správu aplikace. Stylování je řešeno pomocí <span className='bold-text-team'>SCSS</span> pro flexibilní design, a pro ikony používáme <span className='bold-text-team'>FontAwesome</span>. Design aplikace je navržen v <span className='bold-text-team'>Figmě</span>, což nám umožnilo vytvořit moderní a uživatelsky přívětivé prostředí.
+                Celý projekt je zabalen v <span className='bold-text-team'>Dockeru</span>, což nám umožňuje snadné nasazení a správu aplikace. Stylování je řešeno pomocí <span className='bold-text-team'>SCSS</span> pro flexibilní design, a pro ikony používáme <span className='bold-text-team'>FontAwesome</span>. Design aplikace je navržen ve <span className='bold-text-team'>Figmě</span>, což nám umožnilo vytvořit moderní a uživatelsky přívětivé prostředí.
               </p>
 
           </div>
