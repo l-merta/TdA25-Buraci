c7d0ed9e8b8a57254ee6bf5a89909b4559262d1b
l-merta <lukas.merta26@gmail.com>
2025-01-17 22:08:04 +0100

    Online player leave error fix

--- server/online.js
+++ server/online.js
@@ -140,14 +140,14 @@ module.exports = (server) => {
             message: "Zakladatel hry se odpojil"
           });
 
-          if (rooms[roomId].players.length === 1) {
+          if (rooms[roomId].players.length === 0) {
             delete rooms[roomId];
           }
         } 
         else {
           rooms[roomId].players = rooms[roomId].players.filter(client => client.socket !== socket);
 
-          if (rooms[roomId].players.length === 1) {
+          if (rooms[roomId].players.length === 0) {
             delete rooms[roomId];
           } else {
             emitPlayerList(rooms[roomId]);
