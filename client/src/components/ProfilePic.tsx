6afec223c40126e9ede905bd45938c375a965aa1
l-merta <lukas.merta26@gmail.com>
2025-02-25 22:31:53 +0100

    More profile pics

--- client/src/components/ProfilePic.tsx
+++ client/src/components/ProfilePic.tsx
@@ -20,6 +20,7 @@ const ProfilePic: React.FC<ProfilePicProps> = ({ user, index }) => {
         }
       })
       .then(response => {
+        console.log(user.username, response.data.rank);
         setRank(response.data.rank);
       })
       //.catch(error => {});
